d1da8db1ace835d3b0c3584003f655e2f9fec598
aussman <aussman.id@gmail.com>
2021-06-26 14:32:02 +0200

    Add third static variable in loop function

--- queue.ino
+++ queue.ino
@@ -63,6 +63,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   static int num = 0;
   static uint8_t num2 = 0;
+  static uint8_t num3 = 0;
 
   // Try to add item to queue for 10 ticks, fail if queue is full
   if(xQueueSend(msg_queue, (void*)&num, 10) != pdTRUE){
