17304aa9aef2f35bf04eb713db9a8abf6f0114dc
aussman <aussman.id@gmail.com>
2021-06-27 19:31:31 +0200

    Add seventh static variable set to 4

--- queue.ino
+++ queue.ino
@@ -68,6 +68,7 @@ void loop() {
   static uint8_t num4 = 0;
   static uint8_t num5 = 2;
   static uint8_t num6 = 3;
+  static uint8_t num7 = 4;
   
 
   // Try to add item to queue for 10 ticks, fail if queue is full
