0f606557da330266f5d65f5a6f014d618bb62d9b
aussman <aussman.id@gmail.com>
2021-06-27 22:30:12 +0200

    Add a third array: array3 Add a fourth array: array4

--- queue.ino
+++ queue.ino
@@ -22,6 +22,9 @@ static uint8_t num8 = 8;
 static char array1[] = {"BUGATTI"};
 static char array2[] = {"Veyron"};
 
+static char array3[] = {"Chiron"};
+static char array4[] = {"Chiron SuperSport"};
+
 // Globals (queue as a global variable so that all tasks can access it)
 // Creation of the queue
 static QueueHandle_t msg_queue;
